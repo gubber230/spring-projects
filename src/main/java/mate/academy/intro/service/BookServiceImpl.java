de1c632f108455f726d872694214a487b0181b5d
Rostyslav Novak <dbrayn306@gmail.com>
2025-08-26 18:02:19 +0300

    added impls

--- src/main/java/mate/academy/intro/service/BookServiceImpl.java
+++ src/main/java/mate/academy/intro/service/BookServiceImpl.java
@@ -2,17 +2,24 @@ package mate.academy.intro.service;
 
 import java.util.List;
 import mate.academy.intro.model.Book;
+import mate.academy.intro.repository.BookRepositoryImpl;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BookServiceImpl implements BookService {
+    private final BookRepositoryImpl bookRepositoryImpl;
+
+    public BookServiceImpl(BookRepositoryImpl bookRepositoryImpl) {
+        this.bookRepositoryImpl = bookRepositoryImpl;
+    }
+
     @Override
     public Book save(Book book) {
-        return null;
+        return bookRepositoryImpl.save(book);
     }
 
     @Override
     public List<Book> findAll() {
-        return List.of();
+        return bookRepositoryImpl.findAll();
     }
 }
